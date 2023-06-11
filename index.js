acc798d56308cdee27ca73218675a295b9dac2aa
rahatul <rahatul99@gmail.com>
2023-06-11 21:54:42 +0600

    insert a new class implimented

--- index.js
+++ index.js
@@ -192,6 +192,12 @@ async function run() {
             res.send(result);
         })
 
+        app.post('/classes', async(req, res) => {
+            const newClass = req.body;
+            const result = await classesCollection.insertOne(newClass)
+            res.send(result);
+        })
+
         app.get('/whyShould', async (req, res) => {
             const result = await whyShouldCollection.find().toArray();
             res.send(result);
